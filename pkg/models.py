32f3a16dba499f75a8792dd5316fd1e958d467b6
RitiKSIBJr <letter.ritik011@gmail.com>
2024-01-12 22:56:30 +0545

    bigrams and trigrams

--- pkg/models.py
+++ pkg/models.py
@@ -11,9 +11,7 @@ def get_result(filepath):
     for data in cleaned_data:
         result.append(data)
     
-    res = get_unigrams(result)
-    print(res)
-    
+    res = get_trigrams(result)
     return res
 
 
@@ -40,6 +38,57 @@ def get_unigrams(result):
 
     return {'positive':dict(postive_sentiments), 'negative':dict(negative_sentiments), 'neutral':dict(neutral_sentiments)}
 
+def get_bigrams(result):
+    postive_sentiments = defaultdict(int)
+    negative_sentiments = defaultdict(int)
+    neutral_sentiments = defaultdict(int)
+
+    for row in result:
+        if row['sentiment'] == 'positive':
+            for word in generate_n_grams(row['news'],2):
+                postive_sentiments[word] += 1
+
+    for row in result:
+        if row['sentiment'] == 'negative':
+            for word in generate_n_grams(row['news'],2):
+                negative_sentiments[word] += 1
+
+    for row in result:
+        if row['sentiment'] == 'neutral':
+            for word in generate_n_grams(row['news'],2):
+                neutral_sentiments[word] += 1
 
 
+    return {'positive':dict(postive_sentiments), 'negative':dict(negative_sentiments), 'neutral':dict(neutral_sentiments)}
+
+
+def get_trigrams(result):
+    postive_sentiments = defaultdict(int)
+    negative_sentiments = defaultdict(int)
+    neutral_sentiments = defaultdict(int)
+
+    for row in result:
+        if row['sentiment'] == 'positive':
+            for word in generate_n_grams(row['news'],3):
+                postive_sentiments[word] += 1
+
+    for row in result:
+        if row['sentiment'] == 'negative':
+            for word in generate_n_grams(row['news'],3):
+                negative_sentiments[word] += 1
+
+    for row in result:
+        if row['sentiment'] == 'neutral':
+            for word in generate_n_grams(row['news'],3):
+                neutral_sentiments[word] += 1
+
+
+    return {'positive':dict(postive_sentiments), 'negative':dict(negative_sentiments), 'neutral':dict(neutral_sentiments)}
+
+
+def sort_result(result):
+    sorted_positive = dict(sorted(result['positive'].items(), key=lambda item: item[1], reverse=True))
+    sorted_negative = dict(sorted(result['negative'].items(), key=lambda item: item[1], reverse=True))
+    sorted_netural = dict(sorted(result['neutral'].items(), key=lambda item: item[1], reverse=True))
+    return {'positive':sorted_positive, 'negative':sorted_negative, 'neutral':sorted_netural}
 
